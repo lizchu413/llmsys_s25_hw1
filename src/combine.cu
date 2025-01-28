b9fcac3845e65f1a6b4aecd49936599c40eb372c
lizchu413 <lizchu413@gmail.com>
2025-01-28 17:29:25 -0500

    ah

--- src/combine.cu
+++ src/combine.cu
@@ -254,20 +254,22 @@ __global__ void MatrixMultiplyKernel(
         if ((row) < a_shape[1] && (col + tile_idx * TILE) < a_shape[2]) {
             a_shared[thread_x][thread_y] = a_storage[batch * a_batch_stride + (row) * a_strides[1] +
                                                      (col + tile_idx * TILE) * a_strides[2]];
-            printf("tile %d: a_shared[%d][%d] = a_storage[%d][%d] = a_storage[%d]\n", tile_idx,
+            printf("tile %d block [%d][%d]: a_shared[%d][%d] = a_storage[%d][%d] = a_storage[%d]\n", tile_idx, block_x, block_y,
                    thread_x, thread_y, row, col + tile_idx * TILE, batch * a_batch_stride + row * a_strides[1] +(col + tile_idx * TILE) * a_strides[2]);
         } else {
             a_shared[thread_x][thread_y] = 0;
-            printf("tile %d: a_shared[%d][%d] represents a_storage[%d][%d], which is set to zero\n", tile_idx, thread_x, thread_y, row, col + tile_idx * TILE);
+            printf("tile %d block[%d][%d]: a_shared[%d][%d] represents a_storage[%d][%d], which is set to zero\n", tile_idx, block_x, block_y,
+                   thread_x, thread_y, row, col + tile_idx * TILE);
         }
         if ((row + tile_idx * TILE) < b_shape[1] && (col) < b_shape[2]) {
             b_shared[thread_x][thread_y] = b_storage[batch * b_batch_stride + (row + tile_idx * TILE) * b_strides[1] +
                                                      (col) * b_strides[2]];
-            printf("tile %d: b_shared[%d][%d] = b_storage[%d][%d] = b_storage[%d]\n", tile_idx,
+            printf("tile %d block[%d][%d]: b_shared[%d][%d] = b_storage[%d][%d] = b_storage[%d]\n", tile_idx, block_x, block_y,
                    thread_x, thread_y, row + tile_idx * TILE, col, batch * a_batch_stride + (row + tile_idx * TILE) * a_strides[1] + col * a_strides[2]);
         } else {
             b_shared[thread_x][thread_y] = 0;
-            printf("tile %d: b_shared[%d][%d] represents b_storage[%d][%d], which is set to zero\n", tile_idx, thread_x, thread_y, row + tile_idx * TILE, col);
+            printf("tile %d block[%d][%d]: b_shared[%d][%d] represents b_storage[%d][%d], which is set to zero\n", tile_idx, block_x, block_y,
+                   thread_x, thread_y, row + tile_idx * TILE, col);
 
         }
         __syncthreads();
