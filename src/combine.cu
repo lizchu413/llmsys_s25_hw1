bd228da8227f9ef67ad54ba693bd440cb01e9d88
lizchu413 <lizchu413@gmail.com>
2025-01-28 16:17:04 -0500

    ah

--- src/combine.cu
+++ src/combine.cu
@@ -269,20 +269,17 @@ __global__ void MatrixMultiplyKernel(
         }
         __syncthreads();
         // add partial values if the thread we are at is needed
-        if (batch < out_shape[0] && row < out_shape[1] && col < out_shape[2])
-        {
-            for (int k = 0; k < TILE; k++) {
-                if (k < n) {
-                    printf("adding values a_shared[%d][%d] and b_shared[%d][%d] to out[%d][%d]\n", thread_x, k, k, thread_y,thread_x, thread_y );
-                    printf("res before: %d; aval: %d, bval: %d ", res, a_shared[thread_x][k], b_shared[k][thread_y]);
-                    res += a_shared[thread_x][k] * b_shared[k][thread_y];
-                    printf("res after: %d\n", res);
-                }
+        for (int k = 0; k < TILE; k++) {
+            if (k < n) {
+                printf("adding values a_shared[%d][%d] and b_shared[%d][%d] to out[%d][%d]\n", thread_x, k, k, thread_y,thread_x, thread_y );
+                printf("res before: %d; aval: %d, bval: %d ", res, a_shared[thread_x][k], b_shared[k][thread_y]);
+                res += a_shared[thread_x][k] * b_shared[k][thread_y];
+                printf("res after: %d\n", res);
             }
         }
         __syncthreads();
     }
-    out[out_pos] = res;
+    if (batch < out_shape[0] && row < out_shape[1] && col < out_shape[2])  out[out_pos] = res;
     /// END ASSIGN1_2
 }
 
