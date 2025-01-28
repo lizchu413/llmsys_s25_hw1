1f4f5132d64387886751fdb93487a82869f31863
lizchu413 <lizchu413@gmail.com>
2025-01-28 13:45:00 -0500

    ah

--- src/combine.cu
+++ src/combine.cu
@@ -266,6 +266,7 @@ __global__ void MatrixMultiplyKernel(
         __syncthreads();
         // add partial values
         for (int k = 0; k < TILE; k++) {
+            printf("adding values a_shared[%d][%d] and b_shared[%d][%d] to out[%d][%d]\n", thread_y, k, k, thread_x, thread_y, thread_x);
             res += a_shared[thread_y][k] * b_shared[k][thread_x];
         }
         __syncthreads();
