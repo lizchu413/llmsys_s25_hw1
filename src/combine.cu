5df8ef1f3563ef01b07712cbd9661ea116196dfd
lizchu413 <lizchu413@gmail.com>
2025-01-28 16:17:14 -0500

    ah

--- src/combine.cu
+++ src/combine.cu
@@ -270,12 +270,10 @@ __global__ void MatrixMultiplyKernel(
         __syncthreads();
         // add partial values if the thread we are at is needed
         for (int k = 0; k < TILE; k++) {
-            if (k < n) {
-                printf("adding values a_shared[%d][%d] and b_shared[%d][%d] to out[%d][%d]\n", thread_x, k, k, thread_y,thread_x, thread_y );
-                printf("res before: %d; aval: %d, bval: %d ", res, a_shared[thread_x][k], b_shared[k][thread_y]);
-                res += a_shared[thread_x][k] * b_shared[k][thread_y];
-                printf("res after: %d\n", res);
-            }
+            printf("adding values a_shared[%d][%d] and b_shared[%d][%d] to out[%d][%d]\n", thread_x, k, k, thread_y,thread_x, thread_y );
+            printf("res before: %d; aval: %d, bval: %d ", res, a_shared[thread_x][k], b_shared[k][thread_y]);
+            res += a_shared[thread_x][k] * b_shared[k][thread_y];
+            printf("res after: %d\n", res);
         }
         __syncthreads();
     }
