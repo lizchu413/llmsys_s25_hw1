188578192437a356921401c1cf5f2393b1ecfdb8
lizchu413 <lizchu413@gmail.com>
2025-01-28 01:00:20 -0500

    larger

--- src/combine.cu
+++ src/combine.cu
@@ -220,7 +220,7 @@ __global__ void MatrixMultiplyKernel(
    *   None (Fills in out array)
    */
    // TODO DEBUG! DELETE ME AFTER
-   if (out_shape[2] > 3) return;
+   if (out_shape[2] > 20) return;
 
     __shared__ float a_shared[TILE][TILE];
     __shared__ float b_shared[TILE][TILE];
