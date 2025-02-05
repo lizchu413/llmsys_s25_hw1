cb9e535d5c03a68d559d0c259b4cfc9ed11910ec
lizchu413 <lizchu413@gmail.com>
2025-02-05 14:01:35 -0500

    hi

--- project/run_sentiment.py
+++ project/run_sentiment.py
@@ -206,7 +206,7 @@ class SentenceSentimentTrain:
                 x.requires_grad_(True)
                 y.requires_grad_(True)
                 out = model.forward(x)
-                bce = y * out.log() + (-y + 1) * (1 - out).log()
+                bce = y * out.log() + (-y + 1) * (-out + 1).log()
                 loss = -bce.mean()
                 optim.step()
                 # END ASSIGN1_4
